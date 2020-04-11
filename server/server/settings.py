baf20555b5ff55030f97dc832b2ebba9cd550563
Mateusz Mizerski <mateuszmizerski123@gmail.com>
2020-04-11 16:13:40 +0200

    add new models and templates

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'basic_app.apps.BasicAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
