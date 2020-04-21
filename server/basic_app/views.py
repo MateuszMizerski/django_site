597490e0ff05b6c11f15de7e3c8c1d30521d286b
Mateusz Mizerski <mateuszmizerski123@gmail.com>
2020-04-21 19:48:33 +0200

    modifi app (rename databases)

--- server/basic_app/views.py
+++ server/basic_app/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
-from .models import Question
+from .models import Product
 
 
 def basic_view(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    latest_product_list = Product.objects.order_by('-pub_date')[:5]
+    context = {'latest_product_list': latest_product_list}
     return render(request, 'basic_app/basic_view.html', context)
 
 
@@ -15,15 +15,15 @@ def second_view(request):
     return HttpResponse("This is second view")
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'basic_app/detail.html', {'question': question})
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'basic_app/detail.html', {'product': product})
 
 
-def results(request, question_id):
+def results(request, product_id):
     response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    return HttpResponse(response % product_id)
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, product_id):
+    return HttpResponse("You're voting on question %s." % product_id)
